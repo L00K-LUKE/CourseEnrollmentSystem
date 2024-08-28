5bfece263f102a086761d43538e1274d533935d8
Luke <149436181+L00K-LUKE@users.noreply.github.com>
2024-08-28 15:11:27 +0100

    Overloaded constructor to create courses with empty schedules, created methods which add and remove classes from the schedule

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Course {
@@ -14,6 +16,13 @@ public class Course {
         this.courseName = courseName;
         this.lecturer = lecturer;
         this.schedule = schedule;
+    }
+
+    public Course(String courseId, String courseName, String lecturer ) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.lecturer = lecturer;
+        this.schedule = new HashSet<ClassSession>();
     }
 
     // Getters
@@ -35,7 +44,11 @@ public class Course {
 
     // Methods
     public void addClassSession(ClassSession session) {
+        schedule.add(session);
+    }
 
+    public void removeClassSession(ClassSession session) {
+        schedule.remove(session);
     }
 
 }
