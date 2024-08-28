9f1c3461f71ec8317709c1e856726a8423cd7cab
Luke <149436181+L00K-LUKE@users.noreply.github.com>
2024-08-28 15:31:21 +0100

    implemented the timetable class

--- src/main/java/Timetable.java
+++ src/main/java/Timetable.java
@@ -1,6 +1,38 @@
 package main.java;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Timetable {
 
+    Set<ClassSession> sessions;
 
+    public Timetable() {
+        this.sessions = new HashSet<ClassSession>();
+    }
+
+    // Methods
+
+    public void addSession(ClassSession session) {
+        sessions.add(session);
+    }
+
+    public void removeSession(ClassSession session) {
+            sessions.remove(session);
+    }
+
+    public boolean checkClash(ClassSession newSession) {
+
+        for (ClassSession existingClass : sessions) {
+            if (existingClass.conflictsWith(newSession)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addCourse(Course course) {
+        Set<ClassSession> schedule = course.getSchedule();
+        schedule.forEach(this::addSession);
+    }
 }
