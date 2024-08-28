5213b04dfd7491782701bb0b0981b3ec994d79eb
Luke <149436181+L00K-LUKE@users.noreply.github.com>
2024-08-28 15:25:35 +0100

    changed constructor, getters, and added setTitle method. Implemented conflicts with method and get details

--- src/main/java/ClassSession.java
+++ src/main/java/ClassSession.java
@@ -1,6 +1,8 @@
 package main.java;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Set;
 
 public abstract class ClassSession {
     private DaysOfWeek day;
@@ -8,13 +10,18 @@ public abstract class ClassSession {
     private LocalTime endTime;
     private String location;
     private String tutor;
+    private Course course;
+    private String title;
 
-    public ClassSession(DaysOfWeek day, LocalTime startTime, LocalTime endTime, String location, String tutor) {
+    public ClassSession(DaysOfWeek day, LocalTime startTime, LocalTime endTime, String location, String tutor, Course course) {
         this.day = day;
         this.startTime = startTime;
         this.endTime = endTime;
         this.location = location;
         this.tutor = tutor;
+        this.course = course;
+
+        this.title = null;
     }
 
     // Getters
@@ -38,11 +45,48 @@ public abstract class ClassSession {
         return tutor;
     }
 
-    // Methods
-    public boolean confilctsWith(ClassSession session) {
-
-        return true;
-
+    public Course getCourse() {
+        return course;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    // Setters
+
+    public void setTitle(String str) {
+        this.title = str;
+    }
+
+
+    // Methods
+    public boolean conflictsWith(ClassSession session) {
+        if (this.day != session.getDay()) { // check if sessions on same day
+            return false;
+        }
+
+        // get other sessions times
+        LocalTime otherStartTime = session.getStartTime();
+        LocalTime otherEndTime = session.getEndTime();
+
+        boolean endsBeforeOrAtStart = this.endTime.isBefore(otherStartTime) || this.endTime.equals(otherStartTime);
+        boolean startsAfterOrAtEnd = this.startTime.isAfter(otherEndTime) || this.startTime.equals(otherEndTime);
+
+        // if either is true, return false
+        // else return true
+
+        return !(endsBeforeOrAtStart || startsAfterOrAtEnd);
+    }
+
+    public String getDetails() {
+        String courseName = course.getCourseName();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm a");
+        String details = String.format("COURSE: %s | TYPE: %s |  DAY: %s | TIME: %s  UNTIL %s",
+                courseName, this.title, this.day, this.startTime.format(formatter), this.endTime.format(formatter));
+        return details;
+    }
+
+
 
 }
