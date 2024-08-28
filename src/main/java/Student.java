2bab52bf2b08e74b56cb517e1d1643274185b265
Luke <149436181+L00K-LUKE@users.noreply.github.com>
2024-08-28 15:22:32 +0100

    implemented enroll method in student

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.Set;
+
 public class Student {
 
     private String name;
@@ -33,7 +35,15 @@ public class Student {
 
     // Methods
     public void enroll(Course course) {
-        ;
+        Set<ClassSession> newCourseClasses = course.getSchedule();
+
+        for (ClassSession newSession : newCourseClasses) {
+            if (timetable.checkClash(newSession)) {
+                System.out.println("Clash Detected: " + newSession.getDetails());
+                return;
+            }
+        }
+        timetable.addCourse(course);
     }
 
     public boolean checkTimetableClash(Course course) {
