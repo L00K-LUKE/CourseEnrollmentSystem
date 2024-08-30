f178a3f3989519f8eb38258556fa1b62f1ed0140
Luke <149436181+L00K-LUKE@users.noreply.github.com>
2024-08-30 06:39:30 +0100

    unit tests for Course

---
+++ src/test/java/CourseTest.java
@@ -0,0 +1,40 @@
+package test.java;
+
+import main.java.ClassSession;
+import main.java.Course;
+import main.java.DaysOfWeek;
+import main.java.Lab;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    @Test
+    void addClassSession_shouldAddSessionToSchedule() {
+        Course course = new Course("001a","Algs", "Gus");
+        LocalTime start = LocalTime.of(13,30);
+        LocalTime end = LocalTime.of(15,15);
+        ClassSession lab = new Lab(DaysOfWeek.THURSDAY, start, end, "Red Building", "Gavin", "ALGSa", course);
+
+        course.addClassSession(lab);
+
+        assertEquals(1, course.getSchedule().size());
+    }
+
+    @Test
+    void removeClassSession_shouldRemoveSessionFromSchedule() {
+        Course course = new Course("001a","Algs", "Gus");
+        LocalTime start = LocalTime.of(14,20);
+        LocalTime end = LocalTime.of(15,15);
+
+        ClassSession lab = new Lab(DaysOfWeek.THURSDAY, start, end, "Blue Building", "Mary", "ALGSb", course);
+
+        course.addClassSession(lab);
+        course.removeClassSession(lab);
+
+        assertEquals(0, course.getSchedule().size());
+    }
+}
