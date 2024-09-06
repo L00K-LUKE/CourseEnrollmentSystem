6d5cfe841ecba8efe482bd47a9a42fbe009ae01e
Luke <149436181+L00K-LUKE@users.noreply.github.com>
2024-09-06 17:59:50 +0100

    added automatic id generation

--- src/main/java/courseenrollmentsystem/Lab.java
+++ src/main/java/courseenrollmentsystem/Lab.java
@@ -3,13 +3,14 @@ package courseenrollmentsystem;
 import java.time.LocalTime;
 
 public class Lab extends ClassSession{
+    private static int idIncrementer = 1;
 
-    String labID;
-    String tutor;
+    private String labID;
+    private String tutor;
 
-    public Lab(DaysOfWeek day, LocalTime startTime, LocalTime endTime, String location, String tutor, String labID, Course course) {
+    public Lab(DaysOfWeek day, LocalTime startTime, LocalTime endTime, String location, String tutor, Course course) {
         super(day, startTime, endTime, location, course);
-        this.labID = labID;
+        this.labID = "L" + idIncrementer++;
         this.tutor = tutor;
 
         setTitle("Lab");
--- src/main/java/courseenrollmentsystem/Tutorial.java
+++ src/main/java/courseenrollmentsystem/Tutorial.java
@@ -3,13 +3,14 @@ package courseenrollmentsystem;
 import java.time.LocalTime;
 
 public class Tutorial extends ClassSession{
+    private static int idIncrementer = 1;
 
-    String tutorialId;
-    String tutor;
+    private String tutorialId;
+    private String tutor;
 
-    public Tutorial(DaysOfWeek day, LocalTime startTime, LocalTime endTime, String location, String tutor, String tutorialId, Course course) {
+    public Tutorial(DaysOfWeek day, LocalTime startTime, LocalTime endTime, String location, String tutor, Course course) {
         super(day, startTime, endTime, location, course);
-        this.tutorialId = tutorialId;
+        this.tutorialId = "T" + idIncrementer++;
         this.tutor = tutor;
 
         setTitle("Tutorial");
